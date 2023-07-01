60bbb05d2922d56a2476bc7d3fa5f11ee01fecba
kehindeBankole <bankolek1@gmail.com>
2023-07-01 12:21:21 +0100

    Initial Commit

---
+++ designcode/designcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  designcodeApp.swift
+//  designcode
+//
+//  Created by kehinde on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct designcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
